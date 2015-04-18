202fa355797b30ae9646505cc7dec1d0dda98f1d
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-19 01:18:54 +0700

    View Corlate Template

---
+++ protected/views/corlate/pricing.php
@@ -0,0 +1,133 @@
+<?php $assets = $this->app->assets; $url = $this->app->url; ?>
+    <section class="pricing-page">
+        <div class="container">
+            <div class="center">  
+                <h2>Pricing Table</h2>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
+            </div>  
+            <div class="pricing-area text-center">
+                <div class="row">
+                    <div class="col-sm-4 plan price-one wow fadeInDown">
+                        <ul>
+                            <li class="heading-one">
+                                <h1>Start Up</h1>
+                                <span>$6/Month</span>
+                            </li>
+                            <li>5 Gb Disk Space</li>
+                            <li>1GB Dadicated Ram</li>
+                            <li>10 Addon Domain</li>
+                            <li>10 Email Account</li>
+                            <li>24/7 Support</li>
+                            <li class="plan-action">
+                                <a href="" class="btn btn-primary">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-4 plan price-two wow fadeInDown">
+                        <ul>
+                            <li class="heading-two">
+                                <h1>Standerd</h1>
+                                <span>$12/Month</span>
+                            </li>
+                            <li>10 Gb Disk Space</li>
+                            <li>2GB Dadicated Ram</li>
+                            <li>20 Addon Domain</li>
+                            <li>20 Email Account</li>
+                            <li>24/7 Support</li>
+                            <li class="plan-action">
+                                <a href="" class="btn btn-primary">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-4 plan price-three wow fadeInDown">
+                        <img src="<?php echo $assets->images('ribon_one.png') ?>">
+                        <ul>
+                            <li class="heading-three">
+                                <h1>Premium</h1>
+                                <span>$24/Month</span>
+                            </li>
+                            <li>50 Gb Disk Space</li>
+                            <li>8GB Dadicated Ram</li>
+                            <li>Unlimited Addon Domain</li>
+                            <li>Unlimited Email Account</li>
+                            <li>24/7 Support</li>
+                            <li class="plan-action">
+                                <a href="" class="btn btn-primary">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 plan price-four wow fadeInDown">
+                        <ul>
+                            <li class="heading-four">
+                                <h1>Basic</h1>
+                                <span>$3/Month</span>
+                            </li>
+                            <li>5 Gb Disk Space</li>
+                            <li>1GB Dadicated Ram</li>
+                            <li>10 Addon Domain</li>
+                            <li>10 Email Account</li>
+                            <li>24/7 Support</li>
+                            <li class="plan-action">
+                                <a href="" class="btn btn-primary">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 plan price-five wow fadeInDown">
+                        <ul>
+                            <li class="heading-five">
+                                <h1>Standerd</h1>
+                                <span>$6/Month</span>
+                            </li>
+                            <li>5 Gb Disk Space</li>
+                            <li>1GB Dadicated Ram</li>
+                            <li>10 Addon Domain</li>
+                            <li>10 Email Account</li>
+                            <li>24/7 Support</li>
+                            <li class="plan-action">
+                                <a href="" class="btn btn-primary">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 plan price-six wow fadeInDown">
+                         <img src="<?php echo $assets->images('ribon_two.png') ?>">
+                        <ul>
+                            <li class="heading-six">
+                                <h1>Premium</h1>
+                                <span>$12/Month</span>
+                            </li>
+                            <li>5 Gb Disk Space</li>
+                            <li>1GB Dadicated Ram</li>
+                            <li>10 Addon Domain</li>
+                            <li>10 Email Account</li>
+                            <li>24/7 Support</li>
+                            <li class="plan-action">
+                                <a href="" class="btn btn-primary">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                     <div class="col-sm-6 col-md-3 plan price-seven wow fadeInDown">
+                        <ul>
+                            <li class="heading-seven">
+                                <h1>Developer</h1>
+                                <span>$19/Month</span>
+                            </li>
+                            <li>5 Gb Disk Space</li>
+                            <li>1GB Dadicated Ram</li>
+                            <li>10 Addon Domain</li>
+                            <li>10 Email Account</li>
+                            <li>24/7 Support</li>
+                            <li class="plan-action">
+                                <a href="" class="btn btn-primary">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!--/pricing-area-->
+        </div><!--/container-->
+    </section><!--/pricing-page-->
