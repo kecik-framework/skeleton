1ac73c8a81655a84b69151835d6086bae075eb71
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-19 01:59:05 +0700

    Index SB Admin 2

---
+++ public/sbadmin.php
@@ -0,0 +1,19 @@
+<?php
+require "../vendor/autoload.php";
+
+$dir_config = "../protected/config/";
+
+require($dir_config."debug.php");
+
+$config = require($dir_config."config.php");
+$app = new Kecik\Kecik($config);
+	require($dir_config."assets.php");
+
+	$lib_database = $app->config->get('libraries');
+	if ($lib_database['Database']['enable'])
+		$app->db->connect();
+
+	require($dir_config."container.php");
+	require($dir_config."routes_sb-admin.php");
+
+$app->run();
