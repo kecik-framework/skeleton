04c5eebd6898a050a52425f9ff1f143ba21d5236
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-20 14:04:05 +0700

    add assets and other use controller

--- protected/config/routes.php
+++ protected/config/routes.php
@@ -1,6 +1,12 @@
 <?php
+$app->assets->css->add('bootstrap.min');
+$app->assets->css->add('bootstrap-theme.min');
+$app->assets->css->add('starter-template');
+
 $app->get('/', function() {
-	//return $this->container['welcomeController']->index();
-	$c = new Controller\Welcome($this);
-	return $c->index();
+	return $this->container['welcomeController']->index();
+	//** Direct without Container
+	//$c = new Controller\Welcome($this);
+	//return $c->index();
+	//-- End
 })->template('basic/basic');
