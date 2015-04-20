7da4565f44425bc296ae39822ad8ceea28ec418a
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-20 14:49:11 +0700

    Fixed autoload problem

--- protected/config/third_party.php
+++ protected/config/third_party.php
@@ -7,12 +7,20 @@ class ThirdParty {
 	}
 
 	public static function autoload($class) {
+
 		$class_array = explode('\\', $class);
 		if (count($class_array)>1) {
 			if ($class_array[0] == 'ThirdParty') {
-				$file_load = $this->config->get('path.third_party').'/'.$class_array[1].'/'.$class_array[2].'.php';
-				if (file_exists($file_load))
-					include_once $file_load;
+				$file_load = Config::get('path.third_party').'/'.$class_array[1].'/'.$class_array[2].'.php';
+				if (file_exists($file_load)) {
+					$namespace = "<?php\nnamespace ThirdParty\\$class_array[1];\n?>\n";
+					$thirdpartyfile = fopen($file_load, "r");
+					$thirdpartyclass = fread($thirdpartyfile,filesize($file_load));
+					fclose($thirdpartyfile);
+					eval("?>$namespace.$thirdpartyclass");
+
+					//include_once $file_load;
+				}
 			}
 		}
 	}
