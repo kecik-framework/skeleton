6139ccb2c9400132c988cef6b47befa5c00080dd
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-20 21:49:33 +0700

    add Controller Route like CodeIgniter

--- protected/config/routes.php
+++ protected/config/routes.php
@@ -10,3 +10,9 @@ $app->get('/', function() {
 	//return $c->index();
 	//-- End
 })->template('basic/basic');
+
+$app->get(':controller/:method/:w+', function($controller, $method, $params=[]) {
+	echo $controller = 'Controller\\'.ucfirst($controller);
+	$c = new $controller($this);
+	return call_user_func_array([$c, $method], $params);
+});
