1b3700c858c4605d81b5e4b89fafed4b70b1b303
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-21 20:57:58 +0700

    Change Autoload Third Party to Global Class

--- protected/config/third_party.php
+++ protected/config/third_party.php
@@ -1,27 +1,25 @@
 <?php
 
-class ThirdParty {
+function ThirdPartyAutoload($class) {
+	$class_array = explode('\\', $class);
+	
+	//if (count($class_array)>1) {
+		//if ($class_array[0] == 'ThirdParty') {
 
-	public static function init() {
-		spl_autoload_register(array(self, '::autoload'), true, true);
-	}
+			if (isset($class_array[1]))
+				$file_load = \Kecik\Config::get('path.third_party').'/'.$class_array[0].'/'.$class_array[1].'.php';
+			else
+				$file_load = \Kecik\Config::get('path.third_party').'/'.$class_array[0].'/'.$class_array[0].'.php';
 
-	public static function autoload($class) {
-
-		$class_array = explode('\\', $class);
-		if (count($class_array)>1) {
-			if ($class_array[0] == 'ThirdParty') {
-				$file_load = Config::get('path.third_party').'/'.$class_array[1].'/'.$class_array[2].'.php';
-				if (file_exists($file_load)) {
-					$namespace = "<?php\nnamespace ThirdParty\\$class_array[1];\n?>\n";
-					$thirdpartyfile = fopen($file_load, "r");
-					$thirdpartyclass = fread($thirdpartyfile,filesize($file_load));
-					fclose($thirdpartyfile);
-					eval("?>$namespace.$thirdpartyclass");
-
-					//include_once $file_load;
-				}
+			if (file_exists($file_load)) {
+				include_once $file_load;
+			} else {
+				$file_load = \Kecik\Config::get('path.third_party').'/'.$class_array[0].'.php';
+				if (file_exists($file_load))
+					include_once $file_load;
 			}
-		}
-	}
+		//}
+	//}
 }
+
+spl_autoload_register('ThirdPartyAutoload');
