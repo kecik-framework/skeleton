1652a8eb0d3c477bcc927079785c810c9794badc
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-21 10:08:00 +0700

    fix menu link

--- public/templates/basic/basic.php
+++ public/templates/basic/basic.php
@@ -27,10 +27,10 @@
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="<?php echo ($this->route->is() == '/')? 'active':''?>"><a href="{{ $this->url->to('') }}">Home</a></li>
-            <li class=""><a target="_blank" href="{{ $this->url->baseUrl() }}corlate.php">Corlate</a></li>
-            <li class=""><a target="_blank" href="{{ $this->url->baseUrl() }}margo.php">Margo</a></li>
-            <li class=""><a target="_blank" href="{{ $this->url->baseUrl() }}sbadmin.php">SB Admin 2</a></li>
-            <li class=""><a target="_blank" href="{{ $this->url->baseUrl() }}adminlte.php">AdminLTE</a></li>
+            <li class=""><a target="_blank" href="{{ echo $this->url->baseUrl() }}corlate.php">Corlate</a></li>
+            <li class=""><a target="_blank" href="{{ echo $this->url->baseUrl() }}margo.php">Margo</a></li>
+            <li class=""><a target="_blank" href="{{ echo $this->url->baseUrl() }}sbadmin.php">SB Admin 2</a></li>
+            <li class=""><a target="_blank" href="{{ echo $this->url->baseUrl() }}adminlte.php">AdminLTE</a></li>
             <!-- Add new Menu -->
             <!--
             <li class="<?php echo ($this->route->is() == 'user')? 'active':''?>"><a href="{{ $this->url->to('user') }}">User</a></li>
